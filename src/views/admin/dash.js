e552cc8179e42e2765f9314f3d3c04796cd95530
Marcelo Barbalho <marcelo.barbalho@al.infnet.edu.br>
2020-09-29 08:41:21 -0300

    instalado react-icons

--- src/views/admin/dash.js
+++ src/views/admin/dash.js
@@ -13,10 +13,7 @@ const Dash = () => {
         <Card.Img variant="top" src="holder.js/100px180" />
         <Card.Body>
           <Card.Title>Card Title</Card.Title>
-          <Card.Text>
-            Some quick example text to build on the card title and make up the
-            bulk of the card's content.
-          </Card.Text>
+          <Card.Text>teste teste teste</Card.Text>
           <Button variant="primary">Go somewhere</Button>
         </Card.Body>
       </Card>
