a256bc82f62b5b94bfceff41fd7295f152b89586
Marcelo Barbalho <marcelo.barbalho@al.infnet.edu.br>
2020-09-29 08:39:35 -0300

    instalado react-bootstrap

--- src/views/admin/dash.js
+++ src/views/admin/dash.js
@@ -1,9 +1,25 @@
 import React from "react";
+import { FaAddressBook } from "react-icons/fa";
 
+import { Card, Button } from "react-bootstrap";
 const Dash = () => {
   return (
     <>
-      <h1>rota dash funcionando</h1>
+      <h1>
+        rota dash funcionando
+        <FaAddressBook />
+      </h1>
+      <Card style={{ width: "18rem" }}>
+        <Card.Img variant="top" src="holder.js/100px180" />
+        <Card.Body>
+          <Card.Title>Card Title</Card.Title>
+          <Card.Text>
+            Some quick example text to build on the card title and make up the
+            bulk of the card's content.
+          </Card.Text>
+          <Button variant="primary">Go somewhere</Button>
+        </Card.Body>
+      </Card>
     </>
   );
 };
